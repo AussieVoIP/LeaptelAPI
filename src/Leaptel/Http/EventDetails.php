13b72071146d32e59bfbec8db681ec0124032646
root <xrobau@gmail.com>
2026-07-06 06:16:11 +0000

    Allow events without customer IDs

--- src/Leaptel/Http/EventDetails.php
+++ src/Leaptel/Http/EventDetails.php
@@ -9,16 +9,20 @@ class EventDetails
 {
     public function __invoke(Request $req, string $uuid)
     {
-        $cid = $req->input("cid");
-        if (!$cid) {
-            throw new \Exception("No CID provided");
-        }
         $wh = Webhook::where("uuid", $uuid)->first();
         if (!$wh) {
             throw new \Exception("No event found for $uuid");
         }
-        if ((string) $wh->customer_id !== (string) $cid) {
-            throw new \Exception("Incorrect Customer ID $cid provided");
+        // If there's no customer_id in the webhook, we don't care about
+        // checking if it's valid.
+        if ($wh->customer_id !== null) {
+            $cid = $req->input("cid");
+            if (!$cid) {
+                throw new \Exception("No CID provided");
+            }
+            if ((string) $wh->customer_id !== (string) $cid) {
+                throw new \Exception("Incorrect Customer ID $cid provided");
+            }
         }
         print "<h1>Debug</h1>\n";
         print "<pre>\n";
